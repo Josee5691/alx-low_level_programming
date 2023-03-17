43040ebd5f251f5da815ec436d75fc015eb15fb7
Joseph <josenjoro790@gmail.com>
2023-03-17 14:07:33 -0700

    fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,11 +6,14 @@
  * Return: Always 0 (Success)
  */
 
-int main(void) {
+int main(voi)
+{
     int i, j;
 
-    for (i = 0; i < 100; i++) {
-        for (j = i; j < 100; j++) {
+    for (i = 0; i < 100; i++)
+    {
+        for (j = i; j < 100; j++)
+       	{
             int first_digit_i = i / 10;
             int second_digit_i = i % 10;
             int first_digit_j = j / 10;
@@ -20,7 +23,8 @@ int main(void) {
             putchar(' ');
             putchar(first_digit_j + '0');
             putchar(second_digit_j + '0');
-            if (i != 99 || j != 99) {
+            if (i != 99 || j != 99) 
+	    {
                 putchar(',');
                 putchar(' ');
             }
