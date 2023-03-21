3342b2869f8ff5cb352bce12a43c9e2fc9c6f942
Joseph <josenjoro790@gmail.com>
2023-03-21 04:41:35 -0700

    isAlpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if input is lowercase
+ * @c: variable to determine if input is lowercase
+ * main - uses built in function
+ * Return: 0 if succesful
+ */
+
+int _isalpha(int c)
+{
+	int n = 0;
+
+	if (n >= 'a' && n <= 'z')
+		i = 1;
+	else if (n >= 'A' && n <= 'Z')
+		i = 1;
+	return (i);
+}
