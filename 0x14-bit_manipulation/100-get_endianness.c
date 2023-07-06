cac0acc619b3a11af40b610a820b943714bb0549
Joseph <josephkariuki790@gmail.com>
2023-07-06 12:46:58 -0700

    check andianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main"
+
+/**
+ * get_endianness - checks the andianness
+ * Return: 0 if big endian and 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int val = 1;
+	unsigned char *byte_ptr = (unsigned char *) & value;
+	return (byte_ptr[0] == 1) ? 1 : 0;
+}
