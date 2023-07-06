05a0fde680a0e59853984b732672be2ec9e1cc0d
Joseph <josephkariuki790@gmail.com>
2023-07-06 13:16:31 -0700

    bug fix

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,12 +2,13 @@
 
 /**
  * get_endianness - checks the andianness
- * Return: 0 if big endian and 1 if little endian
+ * Return: ret 0 if big endian and 1 if little endian
  */
 
 int get_endianness(void)
 {
 	unsigned int val = 1;
-	unsigned char *byte_ptr = (unsigned char *) & val;
+	unsigned char *byte_ptr = (unsigned char *) &val;
+
 	return ((byte_ptr[0] == 1) ? 1 : 0);
 }
