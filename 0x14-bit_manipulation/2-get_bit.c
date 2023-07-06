24026d49529ee824200dc39d87477763292def88
Joseph <josephkariuki790@gmail.com>
2023-07-06 05:27:39 -0700

    bug fix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,7 +7,8 @@
   * Return: the value of the bit at index or -1 if error
   */
 
-int get_bit(unsigned long int n, unsigned int index){
+int get_bit(unsigned long int n, unsigned int index)
+{
 	if(index >= sizeof(unsigned long int) * 8)
 		return (-1);
 	return ((n >> index) & 1);
