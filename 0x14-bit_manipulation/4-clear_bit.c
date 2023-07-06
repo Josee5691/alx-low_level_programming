23eeeef9358dd9d1fd4de3385ce63dc99bb2e515
Joseph <josephkariuki790@gmail.com>
2023-07-06 05:47:11 -0700

    bug fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,9 +7,13 @@
   * Return: 1 if it worked or -1 if an error occurred
   */
 
-int clear_bit(unsigned long int *n, unsigned int index){
-	if(n > 63 || !n)
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int mask;
+
+	if (index > 63 || !n)
 		return (-1);
-	*n &= ~(1UL << index);
+	mask = 1 << index;
+	*n = (*n & ~mask) | ((0 << index) & mask);
 	return (1);
 }
