fbaf1b5f78913efff175dd1dacc934b7fd3d41b6
Joseph <josephkariuki790@gmail.com>
2023-08-17 02:28:19 -0700

    add node

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,16 +1,15 @@
 #include "lists.h"
-#include <stdio.h>
 /**
- * print_dlistint - prints all the elements of a dlistint_t list
+ * dlistint_len - count all the elements of a dlistint_t list
  * @h: doubly linked list node
  * Return: the number of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t size = 0;
 
 	for (; h; h = h->next, size++)
-		printf("%d\n", h->n);
+		;
 	return (size);
 }
